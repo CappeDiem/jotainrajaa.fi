b917968727e21a2a9da6eebac1eaf70bf061a6e6
Cappe <42583065+CappeDiem@users.noreply.github.com>
2025-07-18 01:53:55 +0300

    Next version for now

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,15 @@ function App() {
 
   return (
     <div className='flex flex-col h-screen items-center justify-center'>
+      <img src="/rajaa.png" className="h-32 my-8" />
+
       <h1 className='text-6xl'>Hei, Nyt jotain rajaa!</h1>
+
+      <div className="flex flex-row my-2">
+        <a className="m-2 py-2 px-4 bg-sky-900 rounded-md drop-shadow-lg hover:bg-sky-800" href="https://www.maanmittauslaitos.fi/">Mittaa Rajasi</a>
+        <a className="m-2 py-2 px-4 bg-sky-900 rounded-md drop-shadow-lg hover:bg-sky-800" href="https://raja.fi/">Ilmoita rajojen ylittämisestä</a>
+      </div>
+      
     </div>
   )
 }
