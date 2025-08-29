cfc3bffc86615f6b4390e5668b84764112b5fddc
Cappe <42583065+CappeDiem@users.noreply.github.com>
2025-08-29 03:24:15 +0300

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
+import Footer from "./components/Footer"
+
 function App() {
 
   return (
-    <div className='flex flex-col h-screen items-center justify-center'>
-      <img src="/rajaa.png" className="h-32 my-8" />
+    <>
+      <div className='flex flex-col h-screen items-center justify-center'>
+        <img src="/rajaa.png" className="h-32 my-8" />
 
-      <h1 className='text-6xl'>Hei, Nyt jotain rajaa!</h1>
+        <h1 className='text-6xl'>Hei, Nyt jotain rajaa!</h1>
 
-      <div className="flex flex-row my-2">
-        <a className="m-2 py-2 px-4 bg-sky-900 rounded-md drop-shadow-lg hover:bg-sky-800" href="https://www.maanmittauslaitos.fi/">Mittaa Rajasi</a>
-        <a className="m-2 py-2 px-4 bg-sky-900 rounded-md drop-shadow-lg hover:bg-sky-800" href="https://raja.fi/">Ilmoita rajojen ylittämisestä</a>
+        <div className="flex flex-row my-2">
+          <a className="m-2 py-2 px-4 bg-sky-900 rounded-md drop-shadow-lg hover:bg-sky-800" href="https://www.maanmittauslaitos.fi/">Mittaa Rajasi</a>
+          <a className="m-2 py-2 px-4 bg-sky-900 rounded-md drop-shadow-lg hover:bg-sky-800" href="https://raja.fi/">Ilmoita rajojen ylittämisestä</a>
+        </div>
+
       </div>
-      
-    </div>
+      <Footer />
+    </>
   )
 }
 
---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+    return (
+        <div className={"text-center py-4 border-t-2 border-sky-900 bg-neutral-900 w-screen flex flex-row"}>
+            <div className="w-1/3 flex flex-col">
+                <p className={"text-neutral-400"}>
+
+                </p>
+            </div>
+            <div className="w-1/3">
+            </div>
+
+        </div>
+    )
+}
